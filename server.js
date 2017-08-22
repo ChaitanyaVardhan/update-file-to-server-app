aa733fc9ddfc884a48f5d90303b20616aa128a63
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2017-08-22 17:10:00 +0000

    initial code for router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var http = require('http');
 
-function start() {
+var url = require('url');
+
+function start(route) {
     function onRequest(request, response) {
 	console.log("request received");
+        var pathname = url.parse(request.url)['pathname'];
+        route(pathname);
 	response.writeHead(200, {'Content-Type': 'text/plain'})
 	response.write("Hello world from Chaitanya");
 	response.end();        
